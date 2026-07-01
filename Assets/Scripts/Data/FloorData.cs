3e4b523149a6ffff9c60b423f7a70e76bc7227c1
SeRDic0705 <49552752+SeRDic0705@users.noreply.github.com>
2026-07-01 10:20:42 +0900

    fix: 빌드오류 수정

--- Assets/Scripts/Data/FloorData.cs
+++ Assets/Scripts/Data/FloorData.cs
@@ -9,7 +9,7 @@ namespace BeatHero.Data
     {
         [BoxGroup("Floors")]
         [InfoBox("floors[0] = 1층 몬스터, floors[1] = 2층, ... (기획자 작성 예정)")]
-        [ListDrawerSettings(OnBeginListElementGUI = nameof(DrawFloorLabel))]
+        //[ListDrawerSettings(OnBeginListElementGUI = nameof(DrawFloorLabel))]
         public List<MonsterData> floors = new();
 
 #if UNITY_EDITOR
